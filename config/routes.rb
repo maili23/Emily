c294db779104275754f35ddc23d9aa91d9952ec6
Maik <maik.lindner23@gmail.com>
2017-07-05 22:15:45 +0200

    added landing page and order page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  resources :products
+  resources :products, :orders, only: [:index, :show, :create, :destroy]
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
   get 'static_pages/index'
 
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
