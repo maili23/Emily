d2f7fe0e55b5ab719d1d8a229954c5a0562df201
Maik <maik.lindner23@gmail.com>
2017-08-03 22:59:04 +0200

    5.8 adding the star rating

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,16 @@
 class Product < ApplicationRecord
 	has_many :order
+	has_many :comments
 
 	def self.search(search_term)
   	Product.where("name LIKE ?", "%#{search_term}%")
 	end
 
+	def highest_rating_comment
+  	comments.rating_desc.first
+	end
+
+	def average_rating
+  comments.average(:rating).to_f
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
 
   devise_for :users,  :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
 
+  resources :products do
+    resources :comments
+  end
+
   resources :users
 
-  resources :products
-  
   resources :orders
 
   get 'users/index'
