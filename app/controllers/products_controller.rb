456ee379443f12ef49740182af4f2ee05a1960a9
Maik <maik.lindner23@gmail.com>
2017-08-06 21:29:47 +0200

    5.10 Adding Admin flag

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,7 +15,7 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
-    @comments = @product.comments.order("created_at DESC")
+    @comments = @product.comments.order("created_at ASC")
     @comments = @product.comments.paginate(:page => params[:page], :per_page => 10)
   end
 
