71da401e6a56cd2c189ae3d7a18eb29d7146321a
Maik <maik.lindner23@gmail.com>
2017-08-08 20:54:16 +0200

    adding last part of 5.9 Admin Role & Moderating User Comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,5 +4,10 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
+    if user.admin?
+    	can [:manage, :destroy], :all
+    else
+    	can :read, :all
+    end
   end
 end
