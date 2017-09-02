24b8a4251cd01ad66a5516b23d78837140a85c51
Maik <maik.lindner23@gmail.com>
2017-09-02 21:01:01 +0200

    add 6.2 Unit Tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,7 +5,7 @@ describe Product do
 	context "when the product has comments" do
 		
 		let(:product) { Product.create!(name: "Custom style") }
-		let(:user) { User.create!(email: "maili23@web.de") }
+		let(:user) { User.create!(email: "maili23@web.de", password: "password") }
 
 		before do
 			product.comments.create!(rating: 1, user: user, body: "I/'ve seen better bikes!")
@@ -14,7 +14,12 @@ describe Product do
 		end
 
 		it "returns the average rating of all comments" do
-			expect(@product.average_rating).to eq(3)
+			expect(product.average_rating).to eq 3
 		end
+
+		it "is not valid without a name" do
+			expect(Product.new(description: "Nice bike")).not_to be_valid
+		end
+		
 	end
 end
