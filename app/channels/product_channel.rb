d713addd8221424095567ffc0a060e395d27c67b
Maik <maik.lindner23@gmail.com>
2017-10-09 17:00:45 +0200

    6.14 WIP

--- app/channels/product_channel.rb
+++ app/channels/product_channel.rb
@@ -10,4 +10,11 @@ class ProductChannel < ApplicationCable::Channel
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
+
+  def listen (data)
+  	stop_all_streams
+  	stream_for data["product_id"]
+  end
+
+
 end
